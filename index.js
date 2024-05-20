7874ee59ef404bd8a8a6f9ddb33a4fd33a044604
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-05-20 16:21:07 +0530

    Listing Model

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose= require("mongoose");
 const app= express();
 const port= 8080;
 const mongoose_url= "mongodb://127.0.0.1:27017/wanderlust";
+const Listing= require("./models/listing.js");
+
 
 async function main(){
     await mongoose.connect(mongoose_url); 
@@ -10,6 +12,19 @@ async function main(){
 
 main().then(()=>{console.log("Connected to Mongoose")})
 .catch((err)=>{console.log(err)});
+
+app.get("/testListing", async (req, res)=>{
+    let sampleListing= new Listing({
+        title: "My New Villa",
+        description: "By the Beach",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    })
+    await sampleListing.save().then((res)=>{console.log(res)})
+    .catch((err)=>{console.log(err)});
+    res.send("Listing Test Successfully");
+})
 
 app.get("/", (req, res)=>{
     console.log("Index Route Successfully");
