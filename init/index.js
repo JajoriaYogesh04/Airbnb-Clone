aea8c05d9fcbc3b2f0f67aee6a216d5e078dd8d3
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-05-21 12:30:45 +0530

    Initialized Dataset

---
+++ init/index.js
@@ -0,0 +1,20 @@
+const mongoose= require("mongoose");    //mongoDB
+const initData= require("./data");     //Data
+const Listing= require("../models/listing");    //Schema
+const mongoose_url= "mongodb://127.0.0.1:27017/wanderlust";
+
+async function main(){
+    await mongoose.connect(mongoose_url);
+}
+
+main().then((res)=>{console.log("Connected to Data.js")})
+.catch((err)=>{console.log(err)});
+
+const initDB= async ()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data Initialized");
+}
+
+initDB();
+
