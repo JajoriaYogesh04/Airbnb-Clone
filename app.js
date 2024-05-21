00484b143f6501ed62d9ed45ef3f8944878c2ae1
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-05-21 14:11:11 +0530

    Created Index Route

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app= express();
 const port= 8080;
 const mongoose_url= "mongodb://127.0.0.1:27017/wanderlust";
 const Listing= require("./models/listing.js");
+const path= require("path");
 
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
 async function main(){
     await mongoose.connect(mongoose_url); 
@@ -13,17 +16,23 @@ async function main(){
 main().then(()=>{console.log("Connected to Mongoose")})
 .catch((err)=>{console.log(err)});
 
-app.get("/testListing", async (req, res)=>{
-    let sampleListing= new Listing({
-        title: "My New Villa",
-        description: "By the Beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    })
-    await sampleListing.save().then((res)=>{console.log(res)})
-    .catch((err)=>{console.log(err)});
-    res.send("Listing Test Successfully");
+// app.get("/testListing", async (req, res)=>{
+//     let sampleListing= new Listing({
+//         title: "My New Villa",
+//         description: "By the Beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     })
+//     await sampleListing.save().then((res)=>{console.log(res)})
+//     .catch((err)=>{console.log(err)});
+//     res.send("Listing Test Successfully");
+// })
+
+//Index Route
+app.get("/listing", async (req, res)=>{
+    const allListing= await Listing.find({});
+    res.render("index.ejs", { allListing });
 })
 
 app.get("/", (req, res)=>{
