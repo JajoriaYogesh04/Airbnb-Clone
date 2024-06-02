bf2bc53407ee5421646edba09feafed61698c5e7
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-02 13:12:29 +0530

    Using Cookie Parser

--- classroom/server.js
+++ classroom/server.js
@@ -2,17 +2,26 @@ const express= require("express");
 const app= express();
 const users= require("./routes/users");
 const posts= require("./routes/posts");
+const cookieParser= require("cookie-parser");
 
-app.get("/", (req, res)=>{
-    res.send("GET request to root");
-    console.log("GET request to root");
-})
+app.use(cookieParser());
 
 app.get("/getcookies", (req, res)=>{
     res.cookie("Country", "India");
     res.cookie("State", "Rajasthan");
     res.cookie("City", "Jaipur");
-    res.send("hello");
+    res.send("GET COOKIES");
+})
+
+app.get("/", (req, res)=>{
+    res.send("GET request to root");
+    console.log("GET request to root");
+    console.dir(req.cookies);
+})
+
+app.get("/greets", (req, res)=>{
+    let { Name = "anonymous" }= req.cookies;
+    res.send(`Namaste ${ Name }`);
 })
 
 app.use("/users", users);
