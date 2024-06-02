8443f140a9b78617d4f18b1fa9ecc82ec37ad46f
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-02 10:11:02 +0530

    Using Express Router for Listing

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const wrapAsync= require("./utils/wrapAsync.js");
 const ExpressError= require("./utils/expressError.js");
 const { listingSchema, reviewSchema} = require("./schema.js");
 const Review= require("./models/review.js");
+const listings= require("./routes/listing.js")
 
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
@@ -59,111 +60,13 @@ const validateReview = (req, res, next) => {
     }   
 };
 
-// app.get("/testListing", async (req, res)=>{
-//     let sampleListing= new Listing({
-//         title: "My New Villa",
-//         description: "By the Beach",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         country: "India",
-//     })
-//     await sampleListing.save().then((res)=>{console.log(res)})
-//     .catch((err)=>{console.log(err)});
-//     res.send("Listing Test Successfully");
-// })
+app.use("/listing", listings);
 
 //Set up
 app.get("/", (req, res)=>{
     console.log("Index Route Successfully");
     res.send("Index Route Successfully");
 })
-
-//Index Route
-app.get("/listing", wrapAsync(async (req, res)=>{
-    const allListing= await Listing.find({});
-    res.render("listings/index.ejs", { allListing });
-})) 
-
-//New Route
-app.get("/listing/new", (req, res)=>{
-    console.log("Getting New Request");
-    // res.send("Getting New Request");
-    res.render("listings/new.ejs");
-})
-
-//Show Route
-app.get("/listing/:id", wrapAsync(async (req, res)=>{
-    let {id} = req.params;
-    const listing= await Listing.findById(id).populate("review");
-    console.log(`Show request on ID`);
-    // res.send(showData);
-    // console.log(showData);
-    res.render("listings/show.ejs", { listing });
-})) 
-
-//Create Route
-app.post("/listing", validateListing, wrapAsync(async (req, res, next)=>{
-    let listing = req.body.listing;
-    // console.log(listing);
-    // res.send(req.body.listing);
-    // console.log(listing);
-    // if(!listing){
-    //     throw new ExpressError(400, "Send Valid Data For Listing");
-    // }
-    let newListing= new Listing(listing);
-    // if(!newListing.title){
-    //     throw new ExpressError(400, "Send Valid Title For Listing");
-    // }
-    // if(!newListing.description){
-    //     throw new ExpressError(400, "Send Valid Description For Listing");
-    // }
-    // if(!newListing.price){
-    //     throw new ExpressError(400, "Send Valid Price For Listing");
-    // }
-    // if(!newListing.location){
-    //     throw new ExpressError(400, "Send Valid Location For Listing");
-    // }
-    // if(!newListing.country){
-    //     throw new ExpressError(400, "Send Valid Country For Listing");
-    // }
-    await newListing.save()
-    res.redirect("/listing");   
-})
-) 
-
-
-//Edit Route
-app.get("/listing/:id/edit", wrapAsync(async (req, res)=>{
-    let { id }= req.params;
-    // console.log(`Edit request by: ${id}`);
-    let editListing= await Listing.findById(id);
-    console.log(editListing);
-    // res.send(`Edit request by: ${id}`);
-    res.render("listings/edit.ejs", { editListing });
-}))
-
-//Update Route
-app.put("/listing/:id", validateListing, wrapAsync(async (req, res)=>{
-    let { id }= req.params;
-    let editRequest= {...req.body.listing};
-    // console.log(id);
-    // res.send(editRequest);
-    if(!editRequest){
-        throw new ExpressError(400, "Send Valid Data For Listing");
-    }
-    editedListing= await Listing.findByIdAndUpdate(id, editRequest, {new: true, runValidators: true})
-    res.redirect(`/listing/${id}`);
-})) 
-
-//Destroy Route
-app.delete("/listing/:id", wrapAsync(async (req, res)=>{
-    let { id }= req.params;
-    // res.send(id);
-    let deletedListing= await Listing.findByIdAndDelete(id);
-    res.redirect("/listing");
-    console.log(deletedListing);
-})) 
-
 
 // REVIEWS
 // Post Review Route
---
+++ routes/listing.js
@@ -0,0 +1,105 @@
+const express= require("express");
+const router= express.Router();
+const Listing= require("../models/listing.js");
+const wrapAsync= require("../utils/wrapAsync.js");
+const ExpressError= require("../utils/expressError.js");
+const { listingSchema, reviewSchema} = require("../schema.js");
+
+const validateListing = (req, res, next) => {
+    const { error } = listingSchema.validate(req.body);
+    console.log(error);
+    if (error) {
+        const errMsg = error.details.map(el => el.message).join(", ");
+        return next(new ExpressError(400, errMsg));
+    } else {
+        next();
+    }   
+};
+
+//Index Route
+router.get("/", wrapAsync(async (req, res)=>{
+    const allListing= await Listing.find({});
+    res.render("listings/index.ejs", { allListing });
+})) 
+
+//New Route
+router.get("/new", (req, res)=>{
+    console.log("Getting New Request");
+    // res.send("Getting New Request");
+    res.render("listings/new.ejs");
+})
+
+//Show Route
+router.get("/:id", wrapAsync(async (req, res)=>{
+    let {id} = req.params;
+    const listing= await Listing.findById(id).populate("review");
+    console.log(`Show request on ID`);
+    // res.send(showData);
+    // console.log(showData);
+    res.render("listings/show.ejs", { listing });
+})) 
+
+//Create Route
+router.post("/", validateListing, wrapAsync(async (req, res, next)=>{
+    let listing = req.body.listing;
+    // console.log(listing);
+    // res.send(req.body.listing);
+    // console.log(listing);
+    // if(!listing){
+    //     throw new ExpressError(400, "Send Valid Data For Listing");
+    // }
+    let newListing= new Listing(listing);
+    // if(!newListing.title){
+    //     throw new ExpressError(400, "Send Valid Title For Listing");
+    // }
+    // if(!newListing.description){
+    //     throw new ExpressError(400, "Send Valid Description For Listing");
+    // }
+    // if(!newListing.price){
+    //     throw new ExpressError(400, "Send Valid Price For Listing");
+    // }
+    // if(!newListing.location){
+    //     throw new ExpressError(400, "Send Valid Location For Listing");
+    // }
+    // if(!newListing.country){
+    //     throw new ExpressError(400, "Send Valid Country For Listing");
+    // }
+    await newListing.save()
+    res.redirect("/listing");   
+})
+) 
+
+
+//Edit Route
+router.get("/:id/edit", wrapAsync(async (req, res)=>{
+    let { id }= req.params;
+    // console.log(`Edit request by: ${id}`);
+    let editListing= await Listing.findById(id);
+    console.log(editListing);
+    // res.send(`Edit request by: ${id}`);
+    res.render("listings/edit.ejs", { editListing });
+}))
+
+//Update Route
+router.put("/:id", validateListing, wrapAsync(async (req, res)=>{
+    let { id }= req.params;
+    let editRequest= {...req.body.listing};
+    // console.log(id);
+    // res.send(editRequest);
+    if(!editRequest){
+        throw new ExpressError(400, "Send Valid Data For Listing");
+    }
+    editedListing= await Listing.findByIdAndUpdate(id, editRequest, {new: true, runValidators: true})
+    res.redirect(`/listing/${id}`);
+})) 
+
+//Destroy Route
+router.delete("/:id", wrapAsync(async (req, res)=>{
+    let { id }= req.params;
+    // res.send(id);
+    let deletedListing= await Listing.findByIdAndDelete(id);
+    res.redirect("/listing");
+    console.log(deletedListing);
+})) 
+
+module.exports= router;
