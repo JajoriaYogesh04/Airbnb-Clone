b6728bca96b39671f860e5575fdff27e9456a487
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-03 08:50:28 +0530

    Using Express Session

--- classroom/server.js
+++ classroom/server.js
@@ -1,42 +1,49 @@
 const express= require("express");
 const app= express();
-const users= require("./routes/users");
-const posts= require("./routes/posts");
-const cookieParser= require("cookie-parser");
+// const users= require("./routes/users");
+// const posts= require("./routes/posts");
+// const cookieParser= require("cookie-parser");
+const session= require("express-session");
 
-app.use(cookieParser("secretcode"));
+// app.use(cookieParser("secretcode"));
 
-app.get("/getcookies", (req, res)=>{
-    res.cookie("Country", "India");
-    res.cookie("State", "Rajasthan");
-    res.cookie("City", "Jaipur");
-    res.send("GET COOKIES");
+// app.get("/getcookies", (req, res)=>{
+//     res.cookie("Country", "India");
+//     res.cookie("State", "Rajasthan");
+//     res.cookie("City", "Jaipur");
+//     res.send("GET COOKIES");
+// })
+
+// app.get("/getsignedcookies", (req, res)=>{
+//     res.cookie("made-in", "india", {signed: true});
+//     res.send("GET SIGNED COOKIE");
+// })
+
+// app.get("/verify", (req, res)=>{
+//     console.log(req.signedCookies);
+//     res.send("VERIFY");
+// })
+
+// app.get("/", (req, res)=>{
+//     res.send("GET request to root");
+//     console.log("GET request to root");
+//     console.dir(req.cookies);
+// })
+
+// app.get("/greets", (req, res)=>{
+//     let { Name = "anonymous" }= req.cookies;
+//     res.send(`Namaste ${ Name }`);
+// })
+
+// app.use("/users", users);
+
+// app.use("/posts", posts);
+
+app.use(session({secret: "mySecretString"}));
+
+app.get("/test",(req, res)=>{
+    res.send("Test Successful!");
 })
-
-app.get("/getsignedcookies", (req, res)=>{
-    res.cookie("made-in", "india", {signed: true});
-    res.send("GET SIGNED COOKIE");
-})
-
-app.get("/verify", (req, res)=>{
-    console.log(req.signedCookies);
-    res.send("VERIFY");
-})
-
-app.get("/", (req, res)=>{
-    res.send("GET request to root");
-    console.log("GET request to root");
-    console.dir(req.cookies);
-})
-
-app.get("/greets", (req, res)=>{
-    let { Name = "anonymous" }= req.cookies;
-    res.send(`Namaste ${ Name }`);
-})
-
-app.use("/users", users);
-
-app.use("/posts", posts);
 
 app.listen(3000, ()=>{
     console.log("Listening to server 3000");
