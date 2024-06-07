eb0c66b873e97ca1c6828761c34d029ef2b60863
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-07 13:58:53 +0530

    Implemented router.route for listings

--- routes/listing.js
+++ routes/listing.js
@@ -1,34 +1,28 @@
 const express= require("express");
 const router= express.Router();
 // const Listing= require("../models/listing.js");
-// const wrapAsync= require("../utils/wrapAsync.js");
-// const ExpressError= require("../utils/expressError.js");
+const wrapAsync= require("../utils/wrapAsync.js");
+const ExpressError= require("../utils/expressError.js");
 // const { listingSchema, reviewSchema} = require("../schema.js");
 // const passport= require('passport');
 const { isLoggedIn, isOwner, validateListing }= require("../middleware.js"); 
 const listingControllers = require("../controllers/listing.js");
 
-
-//Index Route
-router.get("/", listingControllers.index) 
+// Index-Create Route
+router.route("/")
+    .get(listingControllers.index) 
+    .post(validateListing, isLoggedIn, listingControllers.createListing); 
 
 //New Route
 router.get("/new", isLoggedIn, listingControllers.renderNewFrom)
 
-//Show Route
-router.get("/:id", listingControllers.showListing) 
-
-//Create Route
-router.post("/", validateListing, isLoggedIn, listingControllers.createListing) 
-
+// Show-Edit-Destroy Route
+router.route("/:id")
+    .get(listingControllers.showListing) 
+    .put(validateListing, isLoggedIn, isOwner, listingControllers.updateListing) 
+    .delete(isLoggedIn, isOwner, listingControllers.destroyListing) 
 
 //Edit Route
 router.get("/:id/edit", isLoggedIn, isOwner, listingControllers.renderEditForm)
-
-//Update Route
-router.put("/:id", validateListing, isLoggedIn, isOwner, listingControllers.updateListing) 
-
-//Destroy Route
-router.delete("/:id", isLoggedIn, isOwner, listingControllers.destroyListing) 
 
 module.exports= router;
