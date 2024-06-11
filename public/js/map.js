8e92a80538448babf7a53fcba7e28b6580aff0d1
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-11 09:26:42 +0530

    Adding Popup to maker

--- public/js/map.js
+++ public/js/map.js
@@ -2,11 +2,12 @@ console.log(mapToken);
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: coordinates, // starting position [lng, lat]
+    center: listings.geometry.coordinates, // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
 
-// console.log(coordinates);
+// console.log(listings.geometry.coordinates);
 new mapboxgl.Marker({color: "red"})
-.setLngLat(coordinates)
+.setLngLat(listings.geometry.coordinates)
+.setPopup(new mapboxgl.Popup().setHTML(`<h5>${listings.title}</h5><p><i>Exact location will be provided after booking!</i></p>`))
 .addTo(map);
